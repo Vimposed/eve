d2d012a506383a6111c7f58d71b11cfc81e4f815
Kyle <k00benne@gmail.com>
2020-09-08 12:01:50 +0000

    Delete config.ts

--- src/config.ts
+++
@@ -1,4 +0,0 @@
-export const token: string = 'NzUyNzA0MDA3ODc2OTAzMDAz.X1bgPA.rsLIlHx-Spa1-UsqVDQgJaEStxY'
-export const prefix: string = '??'
-export const owners: string[] | string = ['583925649807245322']
-export const mongo: string = 'mongodb+srv://pebblo:drhvdrQ7WcCeVAHs@cluster0-jucev.gcp.mongodb.net/nesquik?retryWrites=true&w=majority'
